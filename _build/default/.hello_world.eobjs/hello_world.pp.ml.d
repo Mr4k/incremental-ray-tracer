856d6246429eea928e4a2c4f27bd2a9fa542439d
Peter Stefek <peterlikesemail@gmail.com>
2020-10-07 17:32:37 -0700

    it compiles!

---
+++ _build/default/.hello_world.eobjs/hello_world.pp.ml.d
@@ -0,0 +1,1 @@
+hello_world.pp.ml: Core Incremental Observer Ppx_module_timer_runtime Var
---
+++ hello_world.ml
@@ -0,0 +1,13 @@
+open Core
+
+module Inc : Incremental.S = Incremental.Make ()
+open Inc
+
+let x = Var.create 13
+let y = Var.create 17
+let z = map2 (Var.watch x) (Var.watch y) ~f:(fun x y -> x + y)
+let z_o = observe z
+
+let () =
+  stabilize ();
+  print_endline (string_of_int (Observer.value_exn z_o))
